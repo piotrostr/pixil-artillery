ac021058fec6cbd15d107358c4563a733e207adf
Piotr <piotr.ostrowski@aol.com>
2021-08-25 21:24:13 +0200

    add algo for cropping weapons from pngs

---
+++ crop_weapon.py
@@ -0,0 +1,29 @@
+import cv2
+
+
+def crop_weapon(img):
+    _img = img.copy()
+    mask = _img[:, :, 3] == 0
+    _img[mask] = [255, 255, 255, 255]
+    _img = cv2.cvtColor(_img, cv2.COLOR_BGRA2BGR)
+    h, w, c = _img.shape
+    to_idx = int(h*0.8)
+    from_idx = int(h*0.25)
+    _img = _img[from_idx: to_idx]
+    h, w, c = _img.shape
+    upper_idx = 0
+    for row in range(h):
+        if _img[row].mean() != 255.:
+            upper_idx = row
+            break
+    left_idx = 0
+    for col in range(w):
+        if _img.transpose(1, 0, 2)[col].mean() != 255.:
+            left_idx = col
+            break
+    lower_idx = h
+    for row in range(h)[::-1]:
+         if _img[row - 1].mean() != 255.:
+            lower_idx = row
+            break
+    return img[from_idx + upper_idx: from_idx + lower_idx, left_idx:]
