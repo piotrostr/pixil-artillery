83bb1686650e9a57bbb72d682f1c94801b6670c7
Piotr <piotr.ostrowski@aol.com>
2021-08-30 21:09:43 +0200

    add properties for two more guns

--- properties.py
+++ properties.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 from jpgize import jpgize
 
 
-# (x, y) - the most bottom points
+# (x, y) - the most bottom points, except for pendants (most upper)
+# todo the piniatas might need to be a unit lower or from the bottom px
 properties = {
     'ak47': {
         'skin': [22, 15],
@@ -18,6 +19,32 @@ properties = {
         'g_launcher': [37, 20],
         'flamethrower': [29, 20],
         'thermal': [17, 7]
+    },
+    'db': {
+        'skin': [16, 4],
+        'pendants': [32, 3],  
+        'red_laser': [31, -1], 
+        'green_laser': [31, 4],
+        'acog': [8, 0],
+        'holo': [7, 0],
+        'red_dot': [7, 0],
+        'suppressor': [46, 3],
+        'g_launcher': [31, 4],
+        'flamethrower': [31, 4],
+        'thermal': [9, 0]
+    },
+    'deagle': {
+        'skin': [16, 4],  # todo
+        'pendants': [21, 6],
+        'red_laser': [20, 0], 
+        'green_laser': [20, 4],
+        'acog': [2, 0],
+        'holo': [2, 0],
+        'red_dot': [3, 0],
+        'suppressor': [28, 4],
+        'g_launcher': [23, 11],
+        'flamethrower': [20, 11],
+        'thermal': [3, -1]
     }
 }
 
