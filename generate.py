a0a2ad19327fa7ac43649b2abba2bcd703ef4180
Piotr <piotr.ostrowski@aol.com>
2021-08-31 16:05:20 +0200

    group files

--- generate.py
+++ generate.py
@@ -2,77 +2,15 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-from properties import properties
-from utils import create_filler, get_bottom_px
 
 
-class Generate:
-    def __init__(
-        self,
-        weapon_name: str, 
-        attachments: list[str],
-        skin: np.ndarray,
-    ):
+class Piece:
+    def __init__(self, weapon: str, attachments: list[str], skin: int):
+        self.weapon = weapon
+        self.nft = self.create_filler(100, 100)
+        self.attachments = attachments
         self.skin = skin
-        self.properties = properties[weapon_name].copy()
-        self.weapon = cv2.imread(f'./weapons/{weapon_name}.png',
-                                 cv2.IMREAD_UNCHANGED)
-        self.attachments = []
-        for name in attachments:
-            attatchment = cv2.imread(f'./attachments/{name}.png', 
-                                     cv2.IMREAD_UNCHANGED)
-            self.attachments.append([name, attatchment])
     
-    def resize(self, dat: np.ndarray, dat_name: str) -> np.ndarray:
-        dx, dy = get_bottom_px(dat)
-        wx, wy = self.properties[dat_name]
-        wh, ww, wc = self.weapon.shape
-        _dat = dat.copy()
-        h, w, c = _dat.shape
-        filler_left = create_filler(h, wx - dx)
-        _dat = np.concatenate((filler_left, _dat), axis=1)
-        h, w, c = _dat.shape
-        filler_bottom = create_filler(wh - wy - 1, w)
-        _dat = np.concatenate((_dat, filler_bottom), axis=0)
-        h, w, c = _dat.shape
-        filler_right = create_filler(h, ww - w)
-        _dat = np.concatenate((_dat, filler_right), axis=1)
-        h, w, c = _dat.shape
-        if wh > h:
-            filler_top = create_filler(wh - h, w)
-            _dat = np.concatenate((filler_top, _dat), axis=0)
-        else:
-            filler_top = create_filler(h - wh, w)
-            self.weapon = np.concatenate((filler_top, self.weapon), axis=0)
-        assert _dat.shape == self.weapon.shape
-        return _dat
-    
-    def widen_weapon_and_update_properties(self):
-        """
-        adds a space for the attachments, translates the points 
-        by the amount of filler added.
-        """
-        fill_size = 60
-        h, w, c = self.weapon.shape
-        filler = create_filler(fill_size, w)
-        self.weapon = np.concatenate((filler, self.weapon), axis=0)
-        for k, [x, y] in self.properties.items():
-            self.properties[k] = [x, y + fill_size]
-            
-        h, w, c = self.weapon.shape
-        filler = create_filler(h, fill_size)
-        self.weapon = np.concatenate((self.weapon, filler), axis=1)
-        
-        h, w, c = self.weapon.shape
-        filler = create_filler(fill_size, w)
-        self.weapon = np.concatenate((self.weapon, filler), axis=0)
-
-        h, w, c = self.weapon.shape
-        filler = create_filler(h, fill_size)
-        self.weapon = np.concatenate((filler, self.weapon), axis=1)
-        for k, [x, y] in self.properties.items():
-            self.properties[k] = [x + fill_size, y]
-            
     def apply(self, dat: np.ndarray):
         assert self.weapon.shape == dat.shape
         assert dat.shape[-1] == 4
@@ -82,29 +20,17 @@ class Generate:
                 if dat[row_idx, px_idx][-1] != 0:
                     self.weapon[row_idx, px_idx] = dat[row_idx, px_idx]
 
+
+    @staticmethod
+	def create_filler(h: int, w: int):
+		row = [[255, 255, 255, 0] for i in range(w)]
+		return np.array([row for i in range(h)]).astype(np.uint8)
+
     def __call__(self):
-        # widen the weapon
-        self.widen_weapon_and_update_properties()
-
-        # apply the skin
-        skin = self.resize(self.skin, 'skin')
-        self.apply(skin)
-
-        # add the attachments
-        for name, attachment in self.attachments:
-            # separate the attachments and their upper-left badegs
-            _attachment = self.resize(attachment, name)
-            self.apply(_attachment)
-
-        # add the caption and scope bits
-        # add background
         plt.imshow(self.weapon)
         plt.show()
     
 
 if __name__ == '__main__':
-    weapon_name = 'ak47'
-    attachments = ['ak47_suppressor', 'holo', 'pinata']
-    skin = cv2.imread('./skins/ak47/Skin 1.png', cv2.IMREAD_UNCHANGED)
-    Generate(weapon_name, attachments, skin)()
+    pass
 
--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 def jpgize(img: np.ndarray):
@@ -10,28 +9,8 @@ def jpgize(img: np.ndarray):
     _img = cv2.cvtColor(_img, cv2.COLOR_BGRA2BGR)
     return _img
 
-
-def get_bottom_px(dat: np.ndarray):
-    _dat = jpgize(dat)
-    white_px = [255, 255, 255]
-    start_idx = None
-    for idx in range(len(_dat[-1])):
-        if not all(i == j for i, j in zip(_dat[-1][idx], white_px)):
-            return (idx, _dat.shape[0])
-
-
 def create_filler(h: int, w: int):
     row = [[255, 255, 255, 0] for i in range(w)]
     return np.array([row for i in range(h)]).astype(np.uint8)
 
-
-def show_px(weapon: np.ndarray, px: list[int, int]):
-    _weapon = jpgize(weapon)
-    _weapon[px[1], px[0]] = [255, 0, 0]
-    fig, ax = plt.subplots(figsize=(20, 20))
-    ax.imshow(_weapon)
-    ax.grid(b=True)
-    ax.xaxis.set_ticks(np.arange(0, _weapon.shape[1], 1))
-    ax.yaxis.set_ticks(np.arange(0, _weapon.shape[0], 1))
-    plt.show()
 
